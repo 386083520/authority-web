dfd2f623922676c113ce430f0ef94e115c765016
386083520 <386083520@qq.com>
2022-02-23 11:38:25 +0800

    登陆界面样式

---
+++ src/views/login.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="login">
+    <el-form ref="loginForm" class="login-form" :model="loginForm">
+      <h3 class="title">若依后台管理系统</h3>
+      <el-form-item prop="username">
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          auto-complete="off"
+          placeholder="账号"
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          v-model="loginForm.password"
+          type="password"
+          auto-complete="off"
+          placeholder="密码"
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="code">
+        <el-input
+          v-model="loginForm.code"
+          auto-complete="off"
+          placeholder="验证码"
+          style="width: 63%"
+        >
+        </el-input>
+        <div class="login-code">
+          <img :src="codeUrl" class="login-code-img"/>
+        </div>
+      </el-form-item>
+      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+      <el-form-item style="width:100%;">
+        <el-button
+          size="medium"
+          type="primary"
+          style="width:100%;"
+        >
+          <span>登 录</span>
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <div class="el-login-footer">
+      <span>Copyright © 2018-2022 ruoyi.vip All Rights Reserved.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      codeUrl: '',
+      loginForm: {
+        username: 'admin',
+        password: 'admin123',
+        rememberMe: false,
+        code: '',
+        uuid: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped  rel="stylesheet/scss" lang="scss">
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url("../assets/images/login-background.jpg");
+  background-size: cover;
+}
+.title {
+  color: #707070;
+  text-align: center;
+  margin: 0px auto 30px auto;
+}
+.login-form {
+  border-radius: 6px;
+  background: #ffffff;
+  width: 400px;
+  padding: 25px 25px 5px 25px;
+}
+.el-login-footer {
+  height: 40px;
+  line-height: 40px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+  letter-spacing: 1px;
+}
+.login-code {
+  width: 33%;
+  height: 38px;
+  float: right;
+}
+.login-code-img {
+  height: 38px;
+}
+</style>
