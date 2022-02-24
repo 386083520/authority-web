51689fa2a1a2871cdcb15d169fad74c2e4021d36
386083520 <386083520@qq.com>
2022-02-24 15:50:28 +0800

    使SvgIcon正常展示图标

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>svgIcon</div>
+  <div v-if="isExternal" :style="styleExternalIcon" class="svg-external-icon svg-icon"/>
+  <svg v-else :class="svgClass"  aria-hidden="true">
+    <use :xlink:href="iconName" />
+  </svg>
 </template>
 
 <script>
+import { isExternal } from '@/utils/validate'
 export default {
-  name: 'SvgIcon'
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isExternal () {
+      return isExternal(this.iconClass)
+    },
+    iconName () {
+      return `#icon-${this.iconClass}`
+    },
+    svgClass () {
+      if (this.className) {
+        return 'svg-icon ' + this.className
+      } else {
+        return 'svg-icon'
+      }
+    },
+    styleExternalIcon () {
+      return {
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .svg-icon {
+    width: 1em;
+    height: 1em;
+    fill: currentColor;
+    vertical-align: -0.15em;
+    overflow: hidden;
+  }
+  .svg-external-icon {}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 module.exports = {
   devServer: {
@@ -13,5 +18,23 @@ module.exports = {
         }
       }
     }
+  },
+  chainWebpack (config) {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/assets/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
   }
 }
