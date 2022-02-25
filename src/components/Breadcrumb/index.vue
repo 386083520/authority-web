26f7bacf942b6b47080a691ad16d2314157c3300
386083520 <386083520@qq.com>
2022-02-25 11:05:19 +0800

    顶部面包屑数据获取

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,15 +1,51 @@
 <template>
   <el-breadcrumb separator="/" class="app-breadcrumb">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
+      <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{ item.meta.title }}</span>
+      <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script>
 export default {
-  name: 'Breadcrumb'
+  name: 'Breadcrumb',
+  watch: {
+    $route (route) {
+      this.getBreadcrumb()
+    }
+  },
+  created () {
+    this.getBreadcrumb()
+  },
+  data () {
+    return {
+      levelList: null
+    }
+  },
+  methods: {
+    getBreadcrumb () {
+      let matched = this.$route.matched.filter(item => item.meta && item.meta.title)
+      const first = matched[0]
+      if (!this.isDashboard(first)) {
+        matched = [{ path: '/index', meta: { title: '首页' } }].concat(matched)
+      }
+      this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
+    },
+    isDashboard (route) {
+      const name = route && route.name
+      if (!name) {
+        return false
+      }
+      return name.trim() === 'Index'
+    },
+    handleLink (item) {
+      const { redirect, path } = item
+      if (redirect) {
+      }
+      this.$router.push(path)
+    }
+  }
 }
 </script>
 
@@ -19,5 +55,9 @@ export default {
     font-size: 14px;
     line-height: 50px;
     margin-left: 8px;
+    .no-redirect {
+      color: #97a8be;
+      cursor: text;
+    }
   }
 </style>
